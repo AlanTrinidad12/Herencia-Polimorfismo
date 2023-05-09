c8576fce2762da3b6e35c1f3f36622c0e5c2dd84
AlanTrinidad12 <129115945+AlanTrinidad12@users.noreply.github.com>
2023-05-08 21:58:09 -0600

    Add files via upload

---
+++ Herencia_Polimorfismo/Herencia_Polimorfismo/Escuela.cs
@@ -0,0 +1,19 @@
+﻿class Escuelas
+{
+    public string Matricula { get; set; }
+    public string Nombre { get; set; }
+    public string ApellidoPaterno { get; set; }
+    public string ApellidoMaterno { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public string CURP { get; set; }
+
+    public Escuelas(string matricula, string nombre, string apellidoPaterno, string apellidoMaterno, DateTime fechaNacimiento, string curp)
+    {
+        Matricula = matricula;
+        Nombre = nombre;
+        ApellidoPaterno = apellidoPaterno;
+        ApellidoMaterno = apellidoMaterno;
+        FechaNacimiento = fechaNacimiento;
+        CURP = curp;
+    }
+}
---
+++ Herencia_Polimorfismo/Herencia_Polimorfismo/Maestro.cs
@@ -0,0 +1,21 @@
+﻿class Maestro : Escuelas
+{
+    public Maestro(string matricula, string nombre, string apellidoPaterno, string apellidoMaterno, DateTime fechaNacimiento, string curp) : base(matricula, nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, curp)
+    {
+        Matricula = matricula;
+        Nombre = nombre;
+        ApellidoPaterno = apellidoPaterno;
+        ApellidoMaterno = apellidoMaterno;
+        FechaNacimiento = fechaNacimiento;
+        CURP = curp;
+    }
+    public void EnseñarDato()
+    {
+        Console.WriteLine("Nombre es: " + Nombre);
+        Console.WriteLine("Matricula es: " + Matricula);
+        Console.WriteLine("Apellido paterno es: " + ApellidoPaterno);
+        Console.WriteLine("Apellido materno es: " + ApellidoMaterno);
+        Console.WriteLine("La CURP es: " + CURP);
+        Console.WriteLine("La fecha de nacimiento es: " + FechaNacimiento);
+    }
+}
